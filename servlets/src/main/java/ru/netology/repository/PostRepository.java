efa3b71a0350593a822b40c0ff26f3f56fa63d29
Victoria <molnisspsss@gmail.com>
2024-10-07 19:12:03 +0300

    Initial commit

---
+++ servlets/src/main/java/ru/netology/repository/PostRepository.java
@@ -0,0 +1,55 @@
+package ru.netology.repository;
+
+import ru.netology.model.Post;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class PostRepository {
+  private final ConcurrentHashMap<Long, Post> posts;
+  private final Lock idLock;
+  private long count = 1;
+
+  public PostRepository() {
+    this.posts = new ConcurrentHashMap<>();
+    this.idLock = new ReentrantLock();
+  }
+
+  public List<Post> all() {
+    return new ArrayList<>(posts.values());
+  }
+
+  public Optional<Post> getById(long id) {
+    return Optional.ofNullable(posts.get(id));
+  }
+
+  public Post save(Post post) {
+    if (post.getId() == 0) {
+      long newId;
+      idLock.lock();
+      try {
+        newId = count++;
+        post.setId(newId);
+      } finally {
+        idLock.unlock();
+      }
+      posts.put(newId, post);
+    } else {
+      posts.compute(post.getId(), (id, existingPost) -> {
+        if (existingPost == null) {
+          throw new IllegalArgumentException("Post with id " + id + " not found");
+        }
+        return post;
+      });
+    }
+    return post;
+  }
+
+  public void removeById(long id) {
+    posts.remove(id);
+  }
+}
---
+++ servlets/src/main/java/ru/netology/servlet/MainServlet.java
@@ -0,0 +1,78 @@
+package ru.netology.servlet;
+
+import ru.netology.controller.PostController;
+import ru.netology.repository.PostRepository;
+import ru.netology.service.PostService;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MainServlet extends HttpServlet {
+  private PostController controller;
+
+  private static final String PATH = "/api/posts";
+  private static final String PATH_D = "/api/posts/\\d+";
+  private static final String GET_METHOD = "GET";
+  private static final String POST_METHOD = "POST";
+  private static final String DELETE_METHOD = "DELETE";
+
+  @Override
+  public void init() {
+    final var repository = new PostRepository();
+    final var service = new PostService(repository);
+    controller = new PostController(service);
+  }
+
+  @Override
+  protected void service(HttpServletRequest req, HttpServletResponse resp) {
+    try {
+      final var path = req.getRequestURI();
+      final var method = req.getMethod();
+
+      if (path.equals("/")) {
+        resp.setStatus(HttpServletResponse.SC_OK);
+      } else if (path.startsWith(PATH)) {
+        switch (method) {
+          case GET_METHOD -> handleGetRequests(path, resp);
+          case POST_METHOD -> handlePostRequests(path, req.getReader(), resp);
+          case DELETE_METHOD -> handleDeleteRequests(path, resp);
+          default -> resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+        }
+      } else {
+        resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+      resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+    }
+  }
+
+
+  private void handleGetRequests(String path, HttpServletResponse resp) throws Exception {
+    if (path.equals(PATH)) {
+      controller.all(resp);
+    } else if (path.matches(PATH_D)) {
+      final var id = Long.parseLong(path.substring(path.lastIndexOf("/") + 1));
+      controller.getById(id, resp);
+    } else {
+      resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+    }
+  }
+
+  private void handlePostRequests(String path, java.io.BufferedReader reader, HttpServletResponse resp) throws Exception {
+    if (path.equals(PATH)) {
+      controller.save(reader, resp);
+    } else {
+      resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+    }
+  }
+
+  private void handleDeleteRequests(String path, HttpServletResponse resp) throws Exception {
+    if (path.matches(PATH_D)) {
+      final var id = Long.parseLong(path.substring(path.lastIndexOf("/") + 1));
+      controller.removeById(id, resp);
+    } else {
+      resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+    }
+  }
+}
